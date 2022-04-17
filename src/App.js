140e459d624b3119ddf4b110df6ac5a018f0eb0c
RidwanSiddique44 <srsiddique118@gmail.com>
2022-04-17 15:19:28 +0600

    rout-setup-done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import './App.css';
-import app from './firebase.init';
-import { getAuth } from "firebase/auth";
-const auth = getAuth(app);
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Home/Home';
+import About from './About/About';
+import Blog from './Blogs/Blog';
+import Checkout from './Checkout/Checkout';
+import NotFound from './NotFound/NotFound';
+import Login from './SignInPages/Login/Login';
+import Signup from './SignInPages/Signup/Signup';
+import RequireAuth from './SignInPages/RequireAuth/RequireAuth';
+
 function App() {
   return (
     <div className="App">
 
-    </div>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path='/blog' element={<Blog></Blog>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/register" element={<Signup></Signup>}></Route>
+        <Route path="/checkout" element={
+          <RequireAuth>
+            <Checkout></Checkout>
+          </RequireAuth>
+        }></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
+      </Routes>
+    </div >
   );
 }
 
